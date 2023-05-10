61435b4a7206bbf0062d3438dc9e28abbbeecf11
Dennis <dennisdao2001@hotmail.com>
2023-05-09 21:12:02 -0400

    Save File Management
    Begin experimenting with Sqlite to see if feasible for save file management, replace the dll for Sqlite as specified in guide for error that was popping up (https://www.youtube.com/watch?v=m43DZbH7bzw)

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,25 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Data;
+using Mono.Data.Sqlite;
 
 namespace UI
 {
+    // Save files based on:
+    // https://www.mongodb.com/developer/code-examples/csharp/saving-data-in-unity3d-using-sqlite/
+
     [DisallowMultipleComponent]
     public class MainMenu : MonoBehaviour {
         [SerializeField]
         [Tooltip("The sound that is played when a button is clicked.")]
         private AudioSource buttonClick;
+        [SerializeField]
+        private int hitCount = 0;
 
-        // Start is called before the first frame update
-        void Start()
-        {
-            
+        public void Start(){
+            // Read values from table
+            IDbConnection dbConnection = CreateAndOpenDatabase();
+            IDbCommand dbCommandReadValues = dbConnection.CreateCommand();
+            dbCommandReadValues.CommandText = "SELECT * FROM SaveFilesTable";
+            IDataReader dataReader = dbCommandReadValues.ExecuteReader();
+
+            while(dataReader.Read()){
+                hitCount = dataReader.GetInt32(1);
+            }
+
+            dbConnection.Close();
         }
 
-        // Update is called once per frame
-        void Update()
-        {
-            
+        public void StartNewGame(){
+
+        }
+
+        public void LoadGame(){
+
         }
 
         public void ExitGame(){
@@ -28,6 +45,20 @@ namespace UI
             #else
                 UnityEngine.Application.Quit();
             #endif
+        }
+
+        private IDbConnection CreateAndOpenDatabase(){
+            // Open connection to database
+            string dbUri = "URI=file:GameData.sqlite";
+            IDbConnection dbConnection = new SqliteConnection(dbUri);
+            dbConnection.Open();
+
+            // Create a table for the save files in the databases if it doesn't exist yet
+            IDbCommand dbCommandCreateTable = dbConnection.CreateCommand();
+            dbCommandCreateTable.CommandText = "CREATE TABLE IF NOT EXISTS SaveFilesTable(id INTEGER PRIMARY KEY, hits INTEGER)";
+            dbCommandCreateTable.ExecuteReader();
+
+            return dbConnection;
         }
     }
 }
